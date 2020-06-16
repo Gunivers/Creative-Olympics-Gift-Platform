8ba0044effd4802c439af61352fd1586a7b5ff94
neil.rahmouni@gmail.com <neil.rahmouni@gmail.com>
2020-06-16 13:31:06 +0200

    Initial commit

---
+++ precache-manifest.4accad45ca3770dde49b49e86bd48815.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7c7c2dbfd487ac3c0b9d0f39070b70ea",
+    "url": "./index.html"
+  },
+  {
+    "revision": "bcdd430129ab51399950",
+    "url": "./static/css/main.3c40bc25.chunk.css"
+  },
+  {
+    "revision": "5d4fdda3345bef64417a",
+    "url": "./static/js/2.4b63b9fa.chunk.js"
+  },
+  {
+    "revision": "eac4cf64bf573541fd6dba29c78393a0",
+    "url": "./static/js/2.4b63b9fa.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bcdd430129ab51399950",
+    "url": "./static/js/main.c37ba506.chunk.js"
+  },
+  {
+    "revision": "40a8a3c8b9ad3aa15991",
+    "url": "./static/js/runtime-main.5f2526f9.js"
+  },
+  {
+    "revision": "5e98861f6ef1ce225121abfb3f418166",
+    "url": "./static/media/getFetch.5e98861f.cjs"
+  }
+]);
